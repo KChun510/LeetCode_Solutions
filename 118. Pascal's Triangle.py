2adfaab70fe0cde7216389cc4cccd9416175bc14
Kyle Chun <106642545+KChun510@users.noreply.github.com>
2023-08-23 14:16:15 -0700

    Create 118. Pascal's Triangle.py
    Beats 99% in runtime and 80% in memory usage

---
+++ 118. Pascal's Triangle.py
@@ -0,0 +1,34 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = []
+        stack = []
+
+        def recur(res, stack, n):
+            if stack == []:
+                stack = [1]
+                res += [stack]
+                recur(res, stack, n+1)
+            elif n == numRows:
+                return res
+            else:
+                prev, nxt = 0, 1
+                tmp = [0] + stack + [0]
+                stack = []
+                while nxt < len(tmp):
+                    stack += [tmp[prev] + tmp[nxt]]
+                    prev += 1
+                    nxt += 1
+                res += [stack]
+                recur(res, stack, n+1)
+        
+        recur(res, stack, 0)
+        return res
+
+
+"""
+Time: O(N^2), for each step of n, our tree width is going to get exponetially larger. More operations.
+Space: O(N^2), One N comes from the recursive stack, and the second N comes from the res which is going tp hold N elements N*N = n^2
+
+"""
+
+            
