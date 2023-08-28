d180aebb25cdf51d5b3b37f8209bb48e2f1c409c
Kyle Chun <106642545+KChun510@users.noreply.github.com>
2023-08-28 16:16:25 -0700

    Update 5. Longest Palindromic Substring.py

--- 5. Longest Palindromic Substring.py
+++ 5. Longest Palindromic Substring.py
@@ -1,14 +1,21 @@
-lass Solution:
+class Solution:
     def longestPalindrome(self, s: str) -> str:
-        dic = {}
+        save = ""
+        biggest_len = 0
         for prev in range(len(s)):
             for nxt in range(prev, len(s)+1):
                 tmp = s[prev:nxt]
-                if tmp == tmp[::-1] and tmp != '' and len(tmp) > 1:
-                    dic[tmp] = len(tmp)
+                if tmp == tmp[::-1] and len(tmp) > biggest_len:
+                    biggest_len = len(tmp)
+                    save = tmp
 
-        if(dic):
-            return max(dic, key=dic.get)
+        if save:
+            return save
         else:
             return s[0]
-        
+
+"""
+time complexity: O(n^3). For every n elements there is a n^2 operations, multiplied by the array slicing which is o(n)
+Space complexity: O(1).  The Only mem allocated it save & biggest_len
+
+"""
