db1fbe85af12945f01fd57957f36c56c8e6d3bb6
kyle chun <kyle_main@dhcp-172-24-186-062.mobile.uci.edu>
2024-11-05 14:00:31 -0800

    My solution to 216 beats 100% of users in speed

---
+++ 216.Combination-sum3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        possible_set = [n for n in range(1,10)]
+        res = []
+
+        def backTrack(currRes, size, target, possible_set):
+            currSum = sum(currRes)
+        
+            if len(currRes) == size and currSum == target:
+                res.append(currRes)
+                return
+            
+            elif len(currRes) >= k:
+                return
+
+            for i in range(len(possible_set)):
+                backTrack(currRes + [possible_set[i]], size, target, possible_set[i+1:])
+
+        if n != 0:
+            backTrack([], k, n, possible_set) 
+
+        return res
+      
